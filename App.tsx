73d8e432e17dbb052ce639a5f3cd3689ce403db6
nobilis <nobilisbenz@gmail.com>
2025-05-05 14:29:29 +0100

    test maroua

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
-import 'react-native-gesture-handler';
+// maroua
+import "react-native-gesture-handler";
 
-import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
@@ -28,3 +31,4 @@ export default function App() {
     </GestureHandlerRootView>
   );
 }
+
